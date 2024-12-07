12f7d2a17223d200b89515443d4d13e5cb248b07
Huaxiao Liang <1184903633@qq.com>
2024-12-07 15:50:42 +0800

    add a cpp_project_example

---
+++ docs/cpp_project_example/FunN.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+  > File Name: Fun.cpp
+  > Author: Huaxiao Liang
+  > Mail: 1184903633@qq.com
+  > Created Time: Sun 06 Oct 2024 02:50:32 PM CST
+ ************************************************************************/
+
+#include "Header.h"
+void ttPrint() { std::cout << "tTest" << std::endl; }
---
+++ docs/cpp_project_example/Main.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+  > File Name: Main.cpp
+  > Author: Huaxiao Liang
+  > Mail: 1184903633@qq.com
+  > Created Time: Sun 06 Oct 2024 02:51:12 PM CST
+ ************************************************************************/
+
+#include "Header.h"
+#include "FunHeader.h"
+int main() {
+   tPrint();
+   ttPrint();
+}
