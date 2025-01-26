23270fc4a525b934490318365f9adc3c7c5de4e7
Huaxiao Liang <1184903633@qq.com>
2025-01-26 14:05:17 +0800

    Optimize the Makefile in performance_comparison_between_gcc_and_clang to achieve the following improvements:    Add descriptive comments for better readability and understanding.    Enable parallel compilation using make -jNumber to improve build efficiency.    Configure Clang/Clang++ libraries in a more general and flexible manner for enhanced portability.    Add more tests to compare their performance.

--- docs/performance_comparison_between_gcc_and_clang/test.sh
+++ docs/performance_comparison_between_gcc_and_clang/test.sh
@@ -5,7 +5,25 @@
 # Created Time: Thu 12 Dec 2024 03:43:17 PM CST
 #########################################################################
 #!/bin/bash
-time ./ggrandom_and_sum.exe
-# time ./gcrandom_and_sum.exe
-time ./cgrandom_and_sum.exe
-time ./ccrandom_and_sum.exe
+if [ -e "ggrandom_and_sum.exe" ]; then
+   echo "ggrandom_and_sum.exe is running:"
+   time ./ggrandom_and_sum.exe
+   echo ""
+fi
+
+if [ -e "gcrandom_and_sum.exe" ]; then
+   echo "gcrandom_and_sum.exe is running:"
+   time ./gcrandom_and_sum.exe
+   echo ""
+fi
+
+if [ -e "cgrandom_and_sum.exe" ]; then
+   echo "cgrandom_and_sum.exe is running:"
+   time ./cgrandom_and_sum.exe
+   echo ""
+fi
+
+if [ -e "ccrandom_and_sum.exe" ]; then
+   echo "ccrandom_and_sum.exe is running:"
+   time ./ccrandom_and_sum.exe
+fi
